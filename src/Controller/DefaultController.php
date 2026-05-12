6f1dd9093180bfb74123448965285e55c0bbff1e
Jan Wieland <info@jan-wieland.de>
2026-05-12 16:19:31 +0200

    fixed path to bundle and code clear

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@ namespace JanWieland\PimAreaBricks\Controller;
 
 use Pimcore\Controller\FrontendController;
 use JanWieland\PimAreaBricks\Service\TemplateService;
-use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,14 +12,6 @@ class DefaultController extends FrontendController
     {
         $viewParams = TemplateService::getViewParams($this->document);
 
-        return $this->render('default/default.html.twig', $viewParams);
-    }
-
-    /**
-     * Forwards the request to admin login
-     */
-    public function loginAction(): Response
-    {
-        return $this->forward(LoginController::class . '::loginCheckAction');
+        return $this->render('@PimAreaBricks/default/default.html.twig', $viewParams);
     }
 }
