12c7aba593f65d0ce4323ff303ce536277c9b2f0
Jan Wieland <info@jan-wieland.de>
2026-05-15 01:58:54 +0200

    removed YAML loader

--- src/PimAreaBricksBundle.php
+++ src/PimAreaBricksBundle.php
@@ -8,34 +8,28 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class PimAreaBricksBundle extends AbstractPimcoreBundle
 {
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
         return "Jan Wieland Pimcore Area Bricks Bundle";
     }
 
+    /**
+     * @return string
+     */
     public function getVersion(): string
     {
         return "1.0.0";
     }
 
+    /**
+     * @return string
+     */
     public function getPath(): string
     {
         return realpath(\dirname(__DIR__));
-    }
-
-    /**
-     * @param ContainerBuilder $container
-     * @return void
-     */
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../config/pimcore')
-        );
-        $loader->load('properties.yaml');
     }
 
     /**
