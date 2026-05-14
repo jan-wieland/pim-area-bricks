77d966281ce4ec0dbbd360b9c8f1b023af569d72
Jan Wieland <info@jan-wieland.de>
2026-05-14 23:43:12 +0200

    added page properties

--- src/PimAreaBricksBundle.php
+++ src/PimAreaBricksBundle.php
@@ -2,6 +2,9 @@
 namespace JanWieland\PimAreaBricks;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class PimAreaBricksBundle extends AbstractPimcoreBundle
 {
@@ -18,5 +21,20 @@ class PimAreaBricksBundle extends AbstractPimcoreBundle
     public function getPath(): string
     {
         return realpath(\dirname(__DIR__));
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return void
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../config/pimcore')
+        );
+        $loader->load('properties.yaml');
     }
 }
