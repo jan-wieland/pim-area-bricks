ca5b266c05310617315dcef1de29e9341fa7c5d9
Jan Wieland <info@jan-wieland.de>
2026-05-15 14:02:51 +0200

    addedtemplate path

--- src/Document/Areabrick/JwpimareasSection.php
+++ src/Document/Areabrick/JwpimareasSection.php
@@ -30,6 +30,14 @@ class JwpimareasSection extends AbstractTemplateAreabrick implements EditableDia
     /**
      * @return string
      */
+    public function getTemplate(): string
+    {
+        return 'templates/areas/jwpimareas-snippet/view.html.twig';
+    }
+
+    /**
+     * @return string
+     */
     public function getIcon(): string
     {
         return '/bundles/pimareabricks/images/editmode/rectangle-outline.svg';
