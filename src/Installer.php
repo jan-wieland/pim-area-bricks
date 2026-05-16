6c4412ebc21d5a8e40d3727daddf5e5644709efe
Jan Wieland <info@jan-wieland.de>
2026-05-16 23:16:43 +0200

    fixed uninstall and added forst CSS

--- src/Installer.php
+++ src/Installer.php
@@ -3,17 +3,29 @@ namespace JanWieland\PimAreaBricks;
 
 use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
 use Pimcore\Model\Property\Predefined;
+use Pimcore\Model\Property\Predefined\Listing as PredefinedListing;
 use Pimcore\Model\Document\DocType;
 
 class Installer extends SettingsStoreAwareInstaller
 {
     private array $propertyKeys = [
+        'jwPimAreas_navMainRoot',
+        'jwPimAreas_hideNavs',
         'jwPimAreas_noIndex',
         'jwPimAreas_noFollow',
         'jwPimAreas_themeColor',
         'jwPimAreas_customTheme',
         'jwPimAreas_jsKey',
         'jwPimAreas_cssKey',
+        'jwPimAreas_headFootInPim',
+    ];
+
+    private array $docTypes = [
+        [
+            'name' => 'Content Page',
+            'controller' => 'JanWieland\PimAreaBricks\Controller\DefaultController::defaultAction',
+            'type' => 'page',
+        ],
     ];
 
     public function install(): void
@@ -34,52 +46,76 @@ class Installer extends SettingsStoreAwareInstaller
     {
         $properties = [
             [
-                'name'        => 'jwPimAreas.predefinedProperties.noIndex.name',
-                'key'         => 'jwPimAreas_noIndex',
-                'type'        => 'bool',
+                'name' => 'jwPimAreas.predefinedProperties.navMainRoot.name',
+                'key' => 'jwPimAreas_navMainRoot',
+                'type' => 'document',
+                'description' => 'jwPimAreas.predefinedProperties.navMainRoot.description',
+                'inheritable' => true,
+                'ctype' => 'document',
+            ],
+            [
+                'name' => 'jwPimAreas.predefinedProperties.hideNavs.name',
+                'key' => 'jwPimAreas_hideNavs',
+                'type' => 'bool',
+                'description' => 'jwPimAreas.predefinedProperties.hideNavs.description',
+                'inheritable' => false,
+                'ctype' => 'document',
+            ],
+            [
+                'name' => 'jwPimAreas.predefinedProperties.noIndex.name',
+                'key' => 'jwPimAreas_noIndex',
+                'type' => 'bool',
                 'description' => 'jwPimAreas.predefinedProperties.noIndex.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
             ],
             [
-                'name'        => 'jwPimAreas.predefinedProperties.noFollow.name',
-                'key'         => 'jwPimAreas_noFollow',
-                'type'        => 'bool',
+                'name' => 'jwPimAreas.predefinedProperties.noFollow.name',
+                'key' => 'jwPimAreas_noFollow',
+                'type' => 'bool',
                 'description' => 'jwPimAreas.predefinedProperties.noFollow.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
             ],
             [
-                'name'        => 'jwPimAreas.predefinedProperties.themeColor.name',
-                'key'         => 'jwPimAreas_themeColor',
-                'type'        => 'text',
+                'name' => 'jwPimAreas.predefinedProperties.themeColor.name',
+                'key' => 'jwPimAreas_themeColor',
+                'type' => 'text',
                 'description' => 'jwPimAreas.predefinedProperties.themeColor.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
             ],
             [
-                'name'        => 'jwPimAreas.predefinedProperties.customTheme.name',
-                'key'         => 'jwPimAreas_customTheme',
-                'type'        => 'text',
+                'name' => 'jwPimAreas.predefinedProperties.customTheme.name',
+                'key' => 'jwPimAreas_customTheme',
+                'type' => 'text',
                 'description' => 'jwPimAreas.predefinedProperties.customTheme.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
             ],
             [
-                'name'        => 'jwPimAreas.predefinedProperties.jsKey.name',
-                'key'         => 'jwPimAreas_jsKey',
-                'type'        => 'text',
+                'name' => 'jwPimAreas.predefinedProperties.jsKey.name',
+                'key' => 'jwPimAreas_jsKey',
+                'type' => 'text',
                 'description' => 'jwPimAreas.predefinedProperties.jsKey.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
             ],
             [
-                'name'        => 'jwPimAreas.predefinedProperties.cssKey.name',
-                'key'         => 'jwPimAreas_cssKey',
-                'type'        => 'text',
+                'name' => 'jwPimAreas.predefinedProperties.cssKey.name',
+                'key' => 'jwPimAreas_cssKey',
+                'type' => 'text',
                 'description' => 'jwPimAreas.predefinedProperties.cssKey.description',
                 'inheritable' => true,
-                'ctype'       => 'document',
+                'ctype' => 'document',
+            ],
+            [
+                'name' => 'jwPimAreas.predefinedProperties.headFootInPim.name',
+                'key' => 'jwPimAreas_headFootInPim',
+                'type' => 'bool',
+                'description' => 'jwPimAreas.predefinedProperties.headFootInPim.description',
+                'inheritable' => true,
+                'ctype' => 'document',
             ],
         ];
 
@@ -97,9 +133,10 @@ class Installer extends SettingsStoreAwareInstaller
 
     private function removePredefinedProperties(): void
     {
-        foreach ($this->propertyKeys as $key) {
-            $property = Predefined::getByKey($key);
-            if ($property instanceof Predefined) {
+        $list = new PredefinedListing();
+        $list->load();
+        foreach ($list->getProperties() as $property) {
+            if (in_array($property->getKey(), $this->propertyKeys)) {
                 $property->delete();
             }
         }
@@ -107,12 +144,14 @@ class Installer extends SettingsStoreAwareInstaller
 
     private function createDocumentTypes(): void
     {
-        $docType = new DocType();
-        $docType->setName('Content Page');
-        $docType->setController('JanWieland\PimAreaBricks\Controller\DefaultController::defaultAction');
-        $docType->setType('page');
-        $docType->setGroup('JWpimAreas');
-        $docType->save();
+        foreach ($this->docTypes as $data) {
+            $docType = new DocType();
+            $docType->setName($data['name']);
+            $docType->setController($data['controller']);
+            $docType->setType($data['type']);
+            $docType->setGroup('JWpimAreas');
+            $docType->save();
+        }
     }
 
     private function removeDocumentTypes(): void
--- src/Service/TemplateService.php
+++ src/Service/TemplateService.php
@@ -14,11 +14,13 @@ class TemplateService
     {
         return [
             'jwPimAreas_navMain' => self::getNavMain($document),
+            'jwPimAreas_hideNavs' => (bool) ($document?->getProperty('jwPimAreas.hideNavs') ?? false),
             'jwPimAreas_noIndex' => $document?->getProperty('jwPimAreas.noIndex'),
             'jwPimAreas_noFollow' => $document?->getProperty('jwPimAreas.noFollow'),
             'jwPimAreas_themeColor' => $document?->getProperty('jwPimAreas.themeColor') ?? '#000',
             'jwPimAreas_jsKey' => $document?->getProperty('jwPimAreas.jsKey'),
             'jwPimAreas_cssKey' => $document?->getProperty('jwPimAreas.cssKey'),
+            'jwPimAreas_headFootInPim' => (bool) ($document?->getProperty('jwPimAreas.headFootInPim') ?? false),
         ];
     }
 
