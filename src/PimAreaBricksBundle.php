77796571c3628e5753248eecbebbe7b70de28941
Jan Wieland <info@jan-wieland.de>
2026-05-23 00:52:38 +0200

    re-added

--- src/PimAreaBricksBundle.php
+++ src/PimAreaBricksBundle.php
@@ -5,9 +5,12 @@ use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
 
-class PimAreaBricksBundle extends AbstractPimcoreBundle
+class PimAreaBricksBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
+    use BundleAdminClassicTrait;
 
     /**
      * @return string
@@ -39,5 +42,27 @@ class PimAreaBricksBundle extends AbstractPimcoreBundle
     public function getInstaller(): Installer
     {
         return $this->container->get(Installer::class);
+    }
+
+    public function getJsPaths(): array
+    {
+        return [
+            '/bundles/pimareabricks/js/admin/theme-context-menu.js'
+        ];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeCssPaths(): array
+    {
+        return [];
     }
 }
