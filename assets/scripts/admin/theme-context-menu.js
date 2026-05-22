59ea4829d5c63dd51c0d8c8241a9f20b707f6f95
Jan Wieland <info@jan-wieland.de>
2026-05-23 00:55:18 +0200

    fixed JS TRY

--- assets/scripts/admin/theme-context-menu.js
+++ assets/scripts/admin/theme-context-menu.js
@@ -1,17 +1,14 @@
-Ext.onReady(function () {
+document.addEventListener('pimcoreReady', function () {
   pimcore.asset.tree.panel.prototype.onNodeContextmenu = pimcore.asset.tree.panel.prototype.onNodeContextmenu.sequence(function (node, event) {
     if (node.data.type !== 'folder') return
-
     var menu = event.contextmenu
     if (!menu) return
-
     menu.add({
       text: t('jwPimAreas.contextMenuTheme.label'),
       iconCls: 'pimcore_icon_asset',
       handler: function () {
         Ext.Msg.confirm(t('jwPimAreas.contextMenuTheme.title'), t('jwPimAreas.contextMenuTheme.folder').replace('%folder%', node.data.text), function (btn) {
           if (btn !== 'yes') return
-
           Ext.Ajax.request({
             url: '/admin/jwAreaBricks/run-theme-import',
             method: 'POST',
@@ -20,7 +17,6 @@ Ext.onReady(function () {
             },
             success: function (response) {
               var result = Ext.decode(response.responseText)
-
               if (result.success) {
                 pimcore.helpers.showNotification(
                   t('jwPimAreas.contextMenuTheme.success.title'),
@@ -35,8 +31,7 @@ Ext.onReady(function () {
                 )
               }
             },
-            failure: function () {
-              // HTTP errors like 500 or 404 etc.
+            failure: function (response) {
               pimcore.helpers.showNotification(
                 t('jwPimAreas.contextMenuTheme.failure.title'),
                 t('jwPimAreas.contextMenuTheme.failure.message').replace('%folder%', node.data.text).replace('%status%', response.status),
