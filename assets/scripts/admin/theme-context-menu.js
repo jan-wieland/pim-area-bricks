e626379101a04cc2881d58b4ab32ffc4f2928474
Jan Wieland <info@jan-wieland.de>
2026-05-23 01:13:51 +0200

    fixed JS TRY

--- assets/scripts/admin/theme-context-menu.js
+++ assets/scripts/admin/theme-context-menu.js
@@ -1,33 +1,33 @@
 document.addEventListener(pimcore.events.prepareAssetTreeContextMenu, function (e) {
   var menu = e.detail.menu
-  var record = e.detail.record
+  var asset = e.detail.asset
 
-  if (record.data.type !== 'folder') return
+  if (asset.data.type !== 'folder') return
 
   menu.add({
     text: t('jwPimAreas.contextMenuTheme.label'),
     iconCls: 'pimcore_icon_asset',
     handler: function () {
-      Ext.Msg.confirm(t('jwPimAreas.contextMenuTheme.title'), t('jwPimAreas.contextMenuTheme.folder').replace('%folder%', record.data.text), function (btn) {
+      Ext.Msg.confirm(t('jwPimAreas.contextMenuTheme.title'), t('jwPimAreas.contextMenuTheme.folder').replace('%folder%', asset.data.text), function (btn) {
         if (btn !== 'yes') return
         Ext.Ajax.request({
           url: '/admin/jwAreaBricks/run-theme-import',
           method: 'POST',
           params: {
-            folderId: record.data.id,
+            folderId: asset.data.id,
           },
           success: function (response) {
             var result = Ext.decode(response.responseText)
             if (result.success) {
               pimcore.helpers.showNotification(
                 t('jwPimAreas.contextMenuTheme.success.title'),
-                t('jwPimAreas.contextMenuTheme.success.message').replace('%folder%', record.data.text),
+                t('jwPimAreas.contextMenuTheme.success.message').replace('%folder%', asset.data.text),
                 'success',
               )
             } else {
               pimcore.helpers.showNotification(
                 t('jwPimAreas.contextMenuTheme.error.title'),
-                t('jwPimAreas.contextMenuTheme.error.message').replace('%folder%', record.data.text).replace('%message%', result.message),
+                t('jwPimAreas.contextMenuTheme.error.message').replace('%folder%', asset.data.text).replace('%message%', result.message),
                 'error',
               )
             }
@@ -35,7 +35,7 @@ document.addEventListener(pimcore.events.prepareAssetTreeContextMenu, function (
           failure: function (response) {
             pimcore.helpers.showNotification(
               t('jwPimAreas.contextMenuTheme.failure.title'),
-              t('jwPimAreas.contextMenuTheme.failure.message').replace('%folder%', record.data.text).replace('%status%', response.status),
+              t('jwPimAreas.contextMenuTheme.failure.message').replace('%folder%', asset.data.text).replace('%status%', response.status),
               'error',
             )
           },
