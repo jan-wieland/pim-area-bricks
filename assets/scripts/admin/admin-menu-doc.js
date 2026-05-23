69b5c7ca4acf6bcec227253741f31f44c9e83764
Jan Wieland <info@jan-wieland.de>
2026-05-24 01:47:01 +0200

    better icon TRY

--- assets/scripts/admin/admin-menu-doc.js
+++ assets/scripts/admin/admin-menu-doc.js
@@ -1,12 +1,12 @@
 document.addEventListener(pimcore.events.preMenuBuild, function (e) {
   e.detail.menu.jwPimAreaBricks = {
     text: t('jwPimAreas.adminMenuDocs.label'),
-    iconCls: 'pimcore_icon_info',
+    iconCls: 'pimcore_icon_import_warning',
     priority: 100,
     items: [
       {
         text: t('jwPimAreas.adminMenuDocs.item'),
-        iconCls: 'pimcore_icon_info',
+        iconCls: 'pimcore_icon_log',
         handler: function () {
           pimcore.helpers.openGenericIframeWindow('jwPimAreas_docs', '/admin/jwAreaBricks/docs', 'pimcore_icon_info', t('jwPimAreas.adminMenuDocs.docs'))
         },
