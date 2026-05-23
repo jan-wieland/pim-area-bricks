54a6841006afca8d3e9a0fedc7c2116b88693bec
Jan Wieland <info@jan-wieland.de>
2026-05-23 18:45:04 +0200

    fixed use

--- src/DependencyInjection/PimAreaBricksExtension.php
+++ src/DependencyInjection/PimAreaBricksExtension.php
@@ -3,6 +3,7 @@ namespace JanWieland\PimAreaBricks\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
