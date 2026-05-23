73705d65b55d7c02aed2de086573d5e9cdbe4be9
Jan Wieland <info@jan-wieland.de>
2026-05-23 22:33:00 +0200

    better label

--- assets/scripts/admin/admin-menu-doc.js
+++ assets/scripts/admin/admin-menu-doc.js
@@ -5,7 +5,7 @@ document.addEventListener(pimcore.events.preMenuBuild, function (e) {
     priority: 100,
     items: [
       {
-        text: t('jwPimAreas.adminMenuDocs.docs'),
+        text: t('jwPimAreas.adminMenuDocs.item'),
         iconCls: 'pimcore_icon_info',
         handler: function () {
           pimcore.helpers.openGenericIframeWindow('jwPimAreas_docs', '/admin/jwAreaBricks/docs', 'pimcore_icon_info', t('jwPimAreas.adminMenuDocs.docs'))
