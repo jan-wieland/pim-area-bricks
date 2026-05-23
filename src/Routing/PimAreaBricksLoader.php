0d962616e21a31b1f2768aa2b741244c389d5976
Jan Wieland <info@jan-wieland.de>
2026-05-23 18:43:19 +0200

    routes thru bundle controller

---
+++ src/Routing/PimAreaBricksLoader.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace JanWieland\PimAreaBricks\Routing;
+
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+class PimAreaBricksLoader extends Loader
+{
+    private bool $loaded = false;
+
+    public function load(mixed $resource, string $type = null): RouteCollection
+    {
+        if ($this->loaded) {
+            throw new \RuntimeException('Do not add this loader twice');
+        }
+
+        $routes = new RouteCollection();
+
+        $routes->add('jw_area_bricks_run_theme_import', new Route(
+            '/admin/jwAreaBricks/run-theme-import',
+            ['_controller' => 'JanWieland\PimAreaBricks\Controller\ThemeBuildController::runThemeImportAction'],
+            [],
+            [],
+            '',
+            [],
+            ['POST']
+        ));
+
+        $this->loaded = true;
+        return $routes;
+    }
+
+    public function supports(mixed $resource, string $type = null): bool
+    {
+        return $type === 'pim_area_bricks';
+    }
+}
