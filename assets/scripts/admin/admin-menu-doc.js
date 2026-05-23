e45ff0e9367040f86b8f634065164b0869bce33d
Jan Wieland <info@jan-wieland.de>
2026-05-23 22:30:48 +0200

    fixed label

--- assets/scripts/admin/admin-menu-doc.js
+++ assets/scripts/admin/admin-menu-doc.js
@@ -1,11 +1,11 @@
 document.addEventListener(pimcore.events.preMenuBuild, function (e) {
   e.detail.menu.jwPimAreaBricks = {
-    label: t('jwPimAreas.adminMenuDocs.label'),
+    text: t('jwPimAreas.adminMenuDocs.label'),
     iconCls: 'pimcore_icon_info',
     priority: 100,
     items: [
       {
-        label: t('jwPimAreas.adminMenuDocs.docs'),
+        text: t('jwPimAreas.adminMenuDocs.docs'),
         iconCls: 'pimcore_icon_info',
         handler: function () {
           pimcore.helpers.openGenericIframeWindow('jwPimAreas_docs', '/admin/jwAreaBricks/docs', 'pimcore_icon_info', t('jwPimAreas.adminMenuDocs.docs'))
