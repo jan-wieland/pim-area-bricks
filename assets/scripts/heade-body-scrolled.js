3a4d1b735a1bd83e17dc4a6b0c6acc77ffc09ce6
Jan Wieland <info@jan-wieland.de>
2026-05-24 23:20:30 +0200

    css and body scrolled

---
+++ assets/scripts/heade-body-scrolled.js
@@ -0,0 +1,45 @@
+let jwScrollSmall = false,
+    jwScrollLast = 0,
+    jwScrollDown = -1,
+    jwScrollMinimum = 0,
+    jwScrollMaximum = 0
+
+document.addEventListener('scroll', () => {
+  let jwScrollPos = window.scrollY
+  if (jwScrollPos !== jwScrollLast && jwScrollPos > 102) {
+    if (jwScrollPos > jwScrollMaximum) {
+      jwScrollMaximum = jwScrollPos
+    }
+    if (jwScrollPos < jwScrollMinimum) {
+      jwScrollMinimum = jwScrollPos
+    }
+    if (scrollDown === -1) {
+      jwScrollDown = 1
+      jwScrollMinimum = jwScrollPos
+      jwScrollMaximum = jwScrollPos
+      jwScrollSmall = true
+      document.body.classList.add('jw-state-scrolled')
+    }
+    if (jwScrollDown === 1) {
+      if (jwScrollSmall && jwScrollPos < (jwScrollMaximum - 68)) {
+        jwScrollDown = 0
+        jwScrollMinimum = jwScrollPos
+        jwScrollSmall = false
+        document.body.classList.remove('jw-state-scrolled')
+      }
+    } else {
+      if (!jwScrollSmall && jwScrollPos > (jwScrollMinimum + 68)) {
+        jwScrollDown = 1
+        jwScrollMaximum = jwScrollPos
+        jwScrollSmall = true
+        document.body.classList.add('jw-state-scrolled')
+      }
+    }
+  }
+  if (scrollSmall && scrollPos < 1) {
+    scrollDown = -1
+    scrollSmall = false
+    document.body.classList.remove('jw-state-scrolled')
+  }
+  scrollLast = scrollPos
+})
