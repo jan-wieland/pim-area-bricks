693a6edb63547a3a0b9d2521a5ccfc12616ee2e1
Jan Wieland <info@jan-wieland.de>
2026-05-28 22:07:26 +0200

    debug relative include path

--- src/DependencyInjection/PimAreaBricksExtension.php
+++ src/DependencyInjection/PimAreaBricksExtension.php
@@ -29,6 +29,7 @@ class PimAreaBricksExtension extends Extension implements PrependExtensionInterf
         $container->prependExtensionConfig('twig', [
             'paths' => [
                 \dirname(__DIR__, 2) . '/templates' => 'PimAreaBricks',
+                \dirname(__DIR__, 2) . '/templates' => null,
             ],
         ]);
 
