0d0b9dd930c0799ff99d62e346e3d52c6895c1be
Jan Wieland <info@jan-wieland.de>
2026-06-07 13:46:37 +0200

    dbuge image data

--- src/Service/OptionsService.php
+++ src/Service/OptionsService.php
@@ -93,7 +93,7 @@ class OptionsService
         if ($this->hasEditables(['imageGeneralhWidth', 'imagePos', 'imagePosRelativeH', 'imageProportion'])) {
             $generalhWidth = $this->getEditable('imageGeneralhWidth')?->getData() ?: null;
 
-            $result->imageData = [
+            $result->imagesData = [
                 'generalhWidth' => is_null($generalhWidth) ? '' : stringf('%spx', $generalhWidth),
                 'imagePos' => $this->getEditable('imagePos')?->getData() ?: 'top-center',
                 'imagePosRelativeH' => $this->getEditable('imagePosRelativeH')?->getData() ?: 'introduction',
