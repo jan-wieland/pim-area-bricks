ae1cb6abab6720fe598e0464884f62c37fc32276
Jan Wieland <info@jan-wieland.de>
2026-06-12 12:48:02 +0200

    first thumbnail DEBUG

--- src/DependencyInjection/PimAreaBricksExtension.php
+++ src/DependencyInjection/PimAreaBricksExtension.php
@@ -42,7 +42,6 @@ class PimAreaBricksExtension extends Extension implements PrependExtensionInterf
             ],
         ]);
         $config = Yaml::parseFile(__DIR__ . '/../../config/pimcore/image-thumbnails.yaml');
-        var_dump(file_get_contents(__DIR__ . '/../../config/pimcore/image-thumbnails.yaml'));
-        #$container->prependExtensionConfig('pimcore', $config['pimcore']);
+        $container->prependExtensionConfig('pimcore', $config['pimcore']);
     }
 }
