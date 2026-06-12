28668831b52f423a19de54890c8d33c37a60d806
Jan Wieland <info@jan-wieland.de>
2026-06-12 12:43:14 +0200

    first thumbnail

--- src/DependencyInjection/PimAreaBricksExtension.php
+++ src/DependencyInjection/PimAreaBricksExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
 class PimAreaBricksExtension extends Extension implements PrependExtensionInterface
 {
